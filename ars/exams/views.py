1046bf66f33f4457acc476061326c93bbb0461aa
Truong Tuan Quang <truong.tuan.quang@framgia.com>
2015-12-15 14:52:16 +0700

    Survey

--- ars/exams/views.py
+++ ars/exams/views.py
@@ -1,9 +1,10 @@
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.views.generic import ListView, FormView
 from django import forms
-from django.forms import formset_factory, modelformset_factory
+from django.forms import modelformset_factory
 from ars.core.views import BaseView
 from ars.exams.models import Exam, Question, Answer
+import traceback
 
 
 class ListExamView(BaseView, ListView):
@@ -23,7 +24,9 @@ class ListExamView(BaseView, ListView):
         return context
 
 
-QuestionFormsetBase = modelformset_factory(Question, fields=('id', 'content',), widgets={'id': forms.HiddenInput}, extra=0, can_order=True)
+QuestionFormsetBase = modelformset_factory(Question, fields=('id', 'content',), widgets={'id': forms.HiddenInput,
+                                                                                         'content': forms.HiddenInput},
+                                           extra=0)
 AnswerFormSet = modelformset_factory(Answer, fields=('id', 'content',), widgets={'id': forms.HiddenInput}, extra=0)
 
 
@@ -32,7 +35,8 @@ class QuestionFormSet(QuestionFormsetBase):
         super(QuestionFormSet, self).add_fields(form, index)
         answer = Answer.objects.filter(question__id=form.initial['id'])
         answer = [(x.id, x.content) for x in answer]
-        form.fields['answer'] = forms.ChoiceField(choices=answer)
+        form.fields['answer'] = forms.ChoiceField(choices=answer,
+                                                  widget=forms.RadioSelect(attrs={'class': 'field'}))
 
 
 class TakeExamView(BaseView, FormView):
@@ -40,14 +44,30 @@ class TakeExamView(BaseView, FormView):
 
     def get_form(self, form_class=None):
         category = self.kwargs['pk']
-        formset = QuestionFormSet(queryset=Question.objects.filter(category=category))
+        if self.request.method == 'POST':
+            formset = QuestionFormSet(self.request.POST, queryset=Question.objects.filter(category=category))
+        else:
+            formset = QuestionFormSet(queryset=Question.objects.filter(category=category))
         return formset
 
-    def form_valid(self, form):
-        pass
+    def get_form_kwargs(self):
+        category = self.kwargs['pk']
+        queryset = Question.objects.filter(category=category)
+        kwargs = super().get_form_kwargs()
+        kwargs['data'] = self.request.POST
+        kwargs['queryset'] = queryset
+        return kwargs
 
-    def form_invalid(self, form):
-        pass
+    def form_valid(self, form):
+        try:
+            for frm in form:
+                answer = Answer.objects.get(id=frm.cleaned_data['answer'])
+                if answer.correct:
+                    total_correct = total_correct + 1
+        except:
+            print(traceback.format_exc())
+
+        return self.render_to_response(self.get_context_data(form=form, total_correct=total_correct))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -59,3 +79,6 @@ class TakeExamView(BaseView, FormView):
         }
         context.update(info)
         return context
+
+    def get_success_url(self):
+        return reverse('exams:index')
